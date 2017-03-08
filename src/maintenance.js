02d37f001419d6782e43b8edac54367ea83ba2a7
David Miller <davidabc66@gmail.com>
2017-03-08 10:59:12 -0500

    old updates deleted after one week instead of one day

--- src/maintenance.js
+++ src/maintenance.js
@@ -2,26 +2,29 @@ const tweetModel = require('./models/tweet.js');
 const incidentModel = require('./models/incident.js');
 
 const DAY = 86400000;
+const WEEK = DAY * 7;
+
 const ONE_DAY_AGO = Date.now() - DAY;
 const TWO_DAYS_AGO = ONE_DAY_AGO - DAY;
+const ONE_WEEK_AGO = Date.now() - WEEK;
 
 const api = {
   deletedOld: () => {
     console.log('Maintenance: Old database data cleared');
     incidentModel
       .find({
-        time: { $lt: TWO_DAYS_AGO }
+        time: { $lt: ONE_WEEK_AGO },
       })
       .remove()
       .exec();
 
     tweetModel
       .find({
-        timestamp_ms: { $lt: TWO_DAYS_AGO }
+        timestamp_ms: { $lt: ONE_WEEK_AGO },
       })
       .remove()
       .exec();
-  }
+  },
 };
 
 module.exports = api;
