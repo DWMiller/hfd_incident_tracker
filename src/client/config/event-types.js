0f634f29213d5806aa1f8bf39bfb853d64b78146
David Miller <davidabc66@gmail.com>
2017-08-01 17:10:34 -0400

    added hydro problem event type

--- src/client/config/event-types.js
+++ src/client/config/event-types.js
@@ -3,112 +3,116 @@ import icons from './icons';
 const types = {
   'CHIMNEY FIRE': {
     text: 'Chimney Fire',
-    icon: icons.FIRE
+    icon: icons.FIRE,
   },
   MEDICAL: {
     text: 'Medical',
-    icon: icons.MEDICAL
+    icon: icons.MEDICAL,
   },
   FIRE: {
     text: 'Fire',
-    icon: icons.FIRE
+    icon: icons.FIRE,
   },
   ALARM: {
     text: 'Alarm',
-    icon: icons.ALARM
+    icon: icons.ALARM,
   },
   'VEHICLE FIRE': {
     text: 'Vehicle Fire',
-    icon: icons.CAR_FIRE
+    icon: icons.CAR_FIRE,
   },
   UNKNOWN: {
     text: 'Unknown Event',
-    icon: icons.UNKNOWN
+    icon: icons.UNKNOWN,
   },
   'ELECTRICAL PROBLEM': {
     text: 'Electrical Problem',
-    icon: icons.ELECTRICAL
+    icon: icons.ELECTRICAL,
+  },
+  'HYDRO PROBLEM': {
+    text: 'Electrical Problem',
+    icon: icons.ELECTRICAL,
   },
   'VEHICLE ACC': {
     text: 'Vehicle Accident',
-    icon: icons.ACCIDENT
+    icon: icons.ACCIDENT,
   },
   FLOODING: {
     text: 'Flooding',
-    icon: icons.FLOODING
+    icon: icons.FLOODING,
   },
   'BURN COMPLAINT': {
     text: 'Burn Victim',
-    icon: icons.MEDICAL
+    icon: icons.MEDICAL,
   },
   'SMOKE DETECTOR': {
     text: 'Smoke Detector',
-    icon: icons.FIRE
+    icon: icons.FIRE,
   },
   SMOKE: {
     text: 'Smoke Reported',
-    icon: icons.FIRE
+    icon: icons.FIRE,
   },
   'GRASS FIRE': {
     text: 'Grass Fire',
-    icon: icons.FIRE
+    icon: icons.FIRE,
   },
   'UNKNOWN FIRE': {
     text: 'Fire',
-    icon: icons.FIRE
+    icon: icons.FIRE,
   },
   'STRUCTURE FIRE': {
     text: 'Structure Fire',
-    icon: icons.FIRE
+    icon: icons.FIRE,
   },
   'RUBBISH FIRE': {
     text: 'Rubbish Fire',
-    icon: icons.FIRE
+    icon: icons.FIRE,
   },
   'APPLIANCE FIRE': {
     text: 'Appliance Fire',
-    icon: icons.FIRE
+    icon: icons.FIRE,
   },
   'ALARM CONDITIONS': {
     text: 'Alarm Conditions',
-    icon: icons.ALARM
+    icon: icons.ALARM,
   },
   'NATURAL GAS': {
     text: 'Natural Gas Leak',
-    icon: icons.ALARM
+    icon: icons.ALARM,
   },
   ODOURS: {
     text: 'Odours Reported',
-    icon: icons.ALARM
+    icon: icons.ALARM,
   },
   'CO DETECTOR': {
     text: 'CO Detector',
-    icon: icons.ALARM
+    icon: icons.ALARM,
   },
   'MULTIPLE ALARM': {
     text: 'Alarms',
-    icon: icons.ALARM
+    icon: icons.ALARM,
   },
   'WATER RESCUE': {
     text: 'Water Rescue',
-    icon: icons.UNKNOWN
+    icon: icons.UNKNOWN,
   },
   'FIRE OUT': {
     text: 'Fire Out',
-    icon: icons.UNKNOWN
+    icon: icons.UNKNOWN,
   },
   'FD ASSISTANCE': {
     text: 'Fire Department Assistance',
-    icon: icons.FIRE_TRUCK
+    icon: icons.FIRE_TRUCK,
   },
   'PROPANE LEAK': {
     text: 'Propane Leak',
-    icon: icons.UNKNOWN
+    icon: icons.UNKNOWN,
   },
   'ROPE RESCUE': {
     text: 'Rope Rescue',
-    icon: icons.UNKNOWN
-  }
+    icon: icons.UNKNOWN,
+  },
 };
 
 export default types;
