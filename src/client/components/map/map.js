f3b99dab632fec10b23b8e569e8cc6a8f8427ade
David Miller <davidabc66@gmail.com>
2018-03-06 08:29:39 -0500

    switched from google-maps-react to react-google-maps

--- src/client/components/map/map.js
+++ src/client/components/map/map.js
@@ -2,26 +2,38 @@ import React, { PureComponent } from 'react';
 
 import { eventListType, genericHandlerType } from '../../types';
 
-import GoogleMap from 'google-map-react';
+import { withScriptjs, withGoogleMap, GoogleMap } from 'react-google-maps';
+
 import MapMarker from '../map_marker/marker';
 
 import './map.css';
 
 const apiKey = 'AIzaSyBDX9TpI_4wnD1Q-JVmLjfhc9B-vPgwc0Y';
+const googleMapURL = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&v=3.exp&libraries=geometry,drawing,places`;
+
 const defaultCenter = { lat: 43.254401, lng: -79.863552 };
 
 class Map extends PureComponent {
-  onEventHover = eventId => {
-    this.props.setActiveEvent(eventId);
+  static propTypes = {
+    mapChange: genericHandlerType,
+    setActiveEvent: genericHandlerType,
+    alerts: eventListType,
   };
 
+  mapRef = React.createRef();
+
+  // onEventHover = eventId => {
+  //   this.props.setActiveEvent(eventId);
+  // };
+
   onMapPropsChange = settings => {
-    this.props.mapChange(settings);
+    console.log(settings);
+    // this.props.mapChange(settings);
   };
 
   generateMarkers(active, alerts) {
     return alerts.map(alert => {
-      const isActive = alert.id === active;
+      const isActive = false; // alert.id === active;
 
       const [lng, lat] = alert.location.coordinates;
 
@@ -40,29 +52,39 @@ class Map extends PureComponent {
 
   render() {
     const Markers = this.generateMarkers(this.props.active, this.props.alerts);
+
+    // onChange={this.onMapPropsChange}
+
     const { zoom, center } = this.props.settings;
 
+    const Map = withScriptjs(
+      withGoogleMap(() => {
+        return (
+          <GoogleMap
+            defaultCenter={defaultCenter}
+            center={center}
+            defaultZoom={10}
+            zoom={zoom}
+            bootstrapURLKeys={{ key: apiKey }}
+            onCenterChanged={this.onMapPropsChange}
+          >
+            {Markers}
+          </GoogleMap>
+        );
+      })
+    );
+
     return (
-      <div className="map-container" width>
-        <GoogleMap
-          onChange={this.onMapPropsChange}
-          defaultCenter={defaultCenter}
-          center={center}
-          defaultZoom={10}
-          zoom={zoom}
-          bootstrapURLKeys={{ key: apiKey }}
-        >
-          {Markers}
-        </GoogleMap>
+      <div className="map-container">
+        <Map
+          googleMapURL={googleMapURL}
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `100%` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        />
       </div>
     );
   }
 }
-
-Map.propTypes = {
-  mapChange: genericHandlerType,
-  setActiveEvent: genericHandlerType,
-  alerts: eventListType,
-};
 
 export default Map;
--- src/client/components/map_marker/marker.js
+++ src/client/components/map_marker/marker.js
@@ -1,33 +1,54 @@
 import React, { PureComponent } from 'react';
+import { Marker, InfoWindow } from 'react-google-maps';
 
 import { genericBooleanType, genericHandlerType, eventType } from '../../types';
+import { eventDefinitions } from '../../config/event-definitions';
 
 import './map-marker.css';
 
-import style from './marker.styles';
+function getIconPath(alert) {
+  let eventType = eventDefinitions[alert.category];
 
-class MapMarker extends PureComponent {
-  onHover = () => {
-    this.props.onEventHover(this.props.alert.id);
-  };
-
-  render() {
-    const { alert, isActive = false } = this.props;
-
-    return (
-      <div
-        className={'event-marker ' + (isActive ? 'active' : '')}
-        onMouseOver={this.onHover}
-        style={style(alert, isActive)}
-      />
-    );
+  if (typeof eventType === 'undefined') {
+    eventType = eventDefinitions.UNKNOWN;
   }
 
+  const icon = eventType.icon;
+
+  return {
+    scaledSize: { height: icon.height, width: icon.width },
+    url: `img/${icon.file}`,
+  };
+}
+
+class MapMarker extends PureComponent {
   static propTypes = {
     isActive: genericBooleanType.isRequired,
     alert: eventType.isRequired,
     onEventHover: genericHandlerType,
   };
+
+  onHover = () => {
+    this.props.onEventHover(this.props.alert.id);
+  };
+
+  render() {
+    // return (
+    //   <div
+    //     className={'event-marker ' + (isActive ? 'active' : '')}
+    //     onMouseOver={this.onHover}
+    //   />
+    // );
+
+    return (
+      <Marker
+        position={{ lat: this.props.lat, lng: this.props.lng }}
+        icon={getIconPath(this.props.alert)}
+      >
+        {this.props.isActive && <InfoWindow />}
+      </Marker>
+    );
+  }
 }
 
 export default MapMarker;
