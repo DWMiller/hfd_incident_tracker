fe8eff8302148a4015bd8ea9bab273311accfe07
David Miller <davidabc66@gmail.com>
2018-03-08 20:11:01 -0500

    fix to error catching functions, weren't updated for koa

--- src/handlers/errorHandlers.js
+++ src/handlers/errorHandlers.js
@@ -7,8 +7,8 @@
 */
 
 exports.catchErrors = fn => {
-  return function(req, res, next) {
-    return fn(req, res, next).catch(next);
+  return function(ctx, next) {
+    return fn(ctx, next).catch(next);
   };
 };
 
@@ -17,7 +17,7 @@ exports.catchErrors = fn => {
 
   If we hit a route that is not found, we mark it as 404 and pass it along to the next error handler to display
 */
-exports.notFound = (req, res, next) => {
+exports.notFound = (ctx, next) => {
   const err = new Error('Not Found');
   err.status = 404;
   next(err);
@@ -42,25 +42,26 @@ exports.notFound = (req, res, next) => {
 
   In development we show good error messages so if we hit a syntax error or any other previously un-handled error, we can show good info on what happened
 */
-exports.developmentErrors = (err, req, res, next) => {
-  err.stack = err.stack || '';
-  const errorDetails = {
-    message: err.message,
-    status: err.status,
-    stackHighlighted: err.stack.replace(
-      /[a-z_-\d]+.js:\d+:\d+/gi,
-      '<mark>$&</mark>'
-    ),
-  };
-  res.status(err.status || 500);
-  res.format({
-    // Based on the `Accept` http header
-    'text/html': () => {
-      res.json(errorDetails);
-      // res.render('error', errorDetails);
-    }, // Form Submit, Reload the page
-    'application/json': () => res.json(errorDetails), // Ajax call, send JSON back
-  });
+exports.developmentErrors = (err, next) => {
+  console.log(err);
+  // err.stack = err.stack || '';
+  // const errorDetails = {
+  //   message: err.message,
+  //   status: err.status,
+  //   stackHighlighted: err.stack.replace(
+  //     /[a-z_-\d]+.js:\d+:\d+/gi,
+  //     '<mark>$&</mark>'
+  //   ),
+  // };
+  // res.status(err.status || 500);
+  // res.format({
+  //   // Based on the `Accept` http header
+  //   'text/html': () => {
+  //     res.json(errorDetails);
+  //     // res.render('error', errorDetails);
+  //   }, // Form Submit, Reload the page
+  //   'application/json': () => res.json(errorDetails), // Ajax call, send JSON back
+  // });
 };
 
 /*
@@ -68,7 +69,7 @@ exports.developmentErrors = (err, req, res, next) => {
 
   No stacktraces are leaked to user
 */
-exports.productionErrors = (err, req, res, next) => {
+exports.productionErrors = (err, next) => {
   res.status(err.status || 500);
   res.json(err);
   // res.render('error', {
