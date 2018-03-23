a00be5af0291763402fe4ab8ec21c43153402cb5
David Miller <davidabc66@gmail.com>
2018-03-23 12:00:45 -0400

    new layout/styles for incident filter panel

--- src/client/components/IncidentFilter/IncidentFilter.js
+++ src/client/components/IncidentFilter/IncidentFilter.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+import FaEye from 'react-icons/lib/fa/eye';
+import FaEyeSlash from 'react-icons/lib/fa/eye-slash';
+
 import icons from '../../config/icons';
 
 import { filterType } from '../../types';
@@ -15,11 +18,6 @@ export class IncidentFilter extends Component {
     selectMultipleIncidentFilters: PropTypes.func.isRequired,
     availableIncidentTypes: PropTypes.arrayOf(PropTypes.string),
   };
-
-  /**
-   * Returns an object containing the present incident types only
-   * object keys are the type icons
-   */
 
   onSelect = type => {
     this.props.toggleIncidentFilter(type);
@@ -39,12 +37,18 @@ export class IncidentFilter extends Component {
       const onSelect = () => this.onSelect(icon);
 
       return (
-        <div
-          key={key}
-          onClick={onSelect}
-          className={'incident-filter-panel-type ' + (isSelected ? 'selected' : '')}
-        >
-          <img alt={'Filter' + icon} src={icons[icon].file} />
+        <div key={key} className={'incident-filter-panel-type ' + (isSelected ? 'selected' : '')}>
+          <img onClick={onSelect} alt={'Filter' + icon} src={icons[icon].file} />
+          <FaEye
+            onClick={!isSelected ? onSelect : null}
+            className={'visibility-icon--on' + (isSelected ? ' active' : '')}
+            style={{ verticalAlign: 'baseline' }}
+          />
+          <FaEyeSlash
+            onClick={isSelected ? onSelect : null}
+            className={'visibility-icon--off' + (!isSelected ? ' active' : '')}
+            style={{ verticalAlign: 'baseline' }}
+          />
         </div>
       );
     });
@@ -52,17 +56,15 @@ export class IncidentFilter extends Component {
 
   render() {
     return (
-      <div className={'incident-filter-panel'}>
-        {this.renderFilterButtons(this.props.availableIncidentTypes)}
-        <button
-          onClick={() => this.selectAll(this.props.availableIncidentTypes)}
-          className={'incident-filter-panel-type'}
-        >
-          All
-        </button>
-        <button onClick={this.deselectAll} className={'incident-filter-panel-type'}>
-          None
-        </button>
+      <div className="incident-filter-panel">
+        <div className="title">Filter Incidents</div>
+        <div className="incident-filter-panel-controls">
+          <button onClick={() => this.selectAll(this.props.availableIncidentTypes)}>All</button>
+          <button onClick={this.deselectAll}>None</button>
+        </div>
+        <div className="incident-filter-panel-types">
+          {this.renderFilterButtons(this.props.availableIncidentTypes)}
+        </div>
       </div>
     );
   }
