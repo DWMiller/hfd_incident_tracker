24eda47c8dcaf9603865bd7fb1ebcf3e09aefa7b
David Miller <davidabc66@gmail.com>
2018-07-01 00:48:20 -0400

    added map to incident page

--- src/client/screens/Incident/Incident.js
+++ src/client/screens/Incident/Incident.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import styled from 'styled-components';
 
 import { TwitterTweetEmbed } from 'react-twitter-embed';
 
 import * as actionCreators from '../../redux/actionCreators';
+
+import ScreenIncidentMap from './Map';
+
+const MapContainerWrapper = styled.div`
+  width: 500px;
+`;
 
 export class ScreenIncident extends Component {
   componentDidMount() {
@@ -12,11 +19,18 @@ export class ScreenIncident extends Component {
   }
 
   render() {
+    // console.log(this.props.incident);
+
     if (!this.props.incident.tweets) {
       return <div />;
     }
     return (
-      <div>{this.props.incident.tweets.map(id => <TwitterTweetEmbed key={id} tweetId={id} />)}</div>
+      <div>
+        <MapContainerWrapper>
+          <ScreenIncidentMap incident={this.props.incident} width="240px" height="240px" />
+        </MapContainerWrapper>
+        {this.props.incident.tweets.map(id => <TwitterTweetEmbed key={id} tweetId={id} />)}
+      </div>
     );
   }
 }
---
+++ src/client/screens/Incident/Map.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { compose, withProps } from 'recompose';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
+
+import { googleMapURL } from './../../config';
+
+export class ScreenIncidentMap extends Component {
+  render() {
+    // const Markers = this.renderMarkers(this.props.active, this.props.incidents);
+    console.log(this.props);
+
+    const [lng, lat] = this.props.incident.location.coordinates;
+
+    return (
+      <GoogleMap
+        defaultCenter={{ lat, lng }}
+        zoom={12}
+        width="240px"
+        height="240px"
+        options={{
+          mapTypeControl: false,
+          streetViewControl: false,
+          zoomControl: false,
+          fullscreenControl: false,
+          disableDoubleClickZoom: true,
+          draggable: false,
+          scrollwheel: false,
+          panControl: false,
+        }}
+      >
+        <Marker position={{ lat, lng }} />
+      </GoogleMap>
+    );
+  }
+}
+
+export default compose(
+  withProps(({ width, height }) => ({
+    googleMapURL,
+    loadingElement: <div style={{ height: `100%` }} />,
+    mapElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ width, height }} />,
+  })),
+  withScriptjs,
+  withGoogleMap
+)(ScreenIncidentMap);
