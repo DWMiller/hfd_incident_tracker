c6aeb71324256cd5e070a7f960d926057414fd03
David Miller <davidabc66@gmail.com>
2020-02-08 00:29:27 -0500

    implements maptiler for map api

--- react-ui/src/components/Map.js
+++ react-ui/src/components/Map.js
@@ -2,12 +2,18 @@ import React, { Component } from 'react';
 
 import PigeonMap from 'pigeon-maps';
 
+const provider = (x, y, z, dpr) => {
+  return `https://api.maptiler.com/maps/basic/${z}/${x}/${y}${
+    dpr >= 2 ? '@2x' : ''
+  }.png?key=4JzO9h50SDZIjSvWYf5g`;
+};
+
 export class Map extends Component {
   render() {
     const { lng, lat, children, ...props } = this.props;
 
     return (
-      <PigeonMap center={[lat, lng]} {...props}>
+      <PigeonMap center={[lat, lng]} {...props} provider={provider}>
         {children}
       </PigeonMap>
     );
