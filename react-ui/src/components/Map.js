82a89aab5979db19950716673def276abfa3946d
David Miller <davidabc66@gmail.com>
2020-02-11 19:43:46 -0500

    switches map tile sizes
    Older api, smaller tiles. Don't think map library has a way to scale with the larger tiles.

--- react-ui/src/components/Map.js
+++ react-ui/src/components/Map.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import PigeonMap from 'pigeon-maps';
 
 const provider = (x, y, z, dpr) => {
-  return `https://api.maptiler.com/maps/basic/${z}/${x}/${y}${
+  return `https://api.maptiler.com/maps/basic/256/${z}/${x}/${y}${
     dpr >= 2 ? '@2x' : ''
   }.png?key=4JzO9h50SDZIjSvWYf5g`;
 };
